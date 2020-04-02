c635363a0157cedd26a79106b52a2905d5de9cf5
Mark Featherston <mark@embeddedarm.com>
2020-04-02 15:03:49 -0700

    ts7970: Removed WIFI test since it is not used in new production

--- board/technologic/ts7970/post.c
+++ board/technologic/ts7970/post.c
@@ -178,40 +178,6 @@ int emmc_test(void)
 	return ret;
 }
 
-int wifi_test(void)
-{
-	static int ret = 0;
-	static bool initialized = 0;
-
-	/* This test only works once per POR, so cache the result */
-	if(!initialized) {
-		uint8_t val;
-		initialized = 1;
-
-		/* This test does not apply to the Silex module */
-		if(board_rev() >= 'F') {
-			ret = 0;
-		} else {
-			/* Wifi is very complex and implementing a real test in u-boot is probably 
-			 * not wise but the bluetooth on the same chip shows sign of life by going low
-			 * after the chip is enabled. */
-			gpio_direction_input(IMX_GPIO_NR(2, 13)); // RTS
-			if(gpio_get_value(IMX_GPIO_NR(2, 13)) != 1)
-				ret = 1;
-
-			val = 3;
-			ret |= i2c_write(0x28, 13, 2, &val, 1);
-			mdelay(500);
-			if(gpio_get_value(IMX_GPIO_NR(2, 13)) != 0)
-				ret = 1;
-		}
-	}
-
-	if (ret == 0) printf("WIFI test passed\n");
-	else printf("WIFI test failed\n");
-	return ret;
-}
-
 /* Check for ISL12020 */
 int rtc_test(void)
 {
@@ -450,7 +416,6 @@ static int do_post_test(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[
 				printf("Silab rev is old or invalid\n");
 			}
 
-			ret |= wifi_test();
 			ret |= usbhub_test();
 			ret |= rtc_test();
 			ret |= marvell_phy_test();
@@ -472,7 +437,6 @@ static int do_post_test(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[
 				printf("Silab rev is old or invalid\n");
 			}
 
-			ret |= wifi_test();
 			ret |= usbhub_test();
 			ret |= rtc_test();
 			ret |= marvell_phy_test();
@@ -493,7 +457,6 @@ static int do_post_test(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[
 				printf("Silab rev is old or invalid\n");
 			}
 
-			ret |= wifi_test();
 			ret |= rtc_test();
 			ret |= marvell_phy_test();
 			break;
@@ -513,7 +476,6 @@ static int do_post_test(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[
 				printf("Silab rev is old or invalid\n");
 			}
 
-			ret |= wifi_test();
 			ret |= rtc_test();
 			ret |= marvell_phy_test();
 			break;
